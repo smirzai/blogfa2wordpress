e8d06410d8f3944594bf85021f91632e274deb92
Saeid Mirzaei <smirzai@gmail.com>
2013-06-25 00:50:19 +0200

    add exporter module

--- blgf2wrdprsSite/blgf2wrdprs/urls.py
+++ blgf2wrdprsSite/blgf2wrdprs/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 
 from blgf2wrdprs import views
 from blgf2wrdprs.views import  contact
+from blgf2wrdprs.views import thanks
 
 urlpatterns = patterns('',
-    url(r'^contact$',  contact)
+    url(r'^contact$',  contact),
+    url(r'^thanks$', thanks)
 )
--- blgf2wrdprsSite/blgf2wrdprs/views.py
+++ blgf2wrdprsSite/blgf2wrdprs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from blgf2wrdprs.forms import ContactForm
+from blgf2wrdprs.exporter.exportBlogfa27  import *
 
 def contact(request):
     if request.method == 'POST': # If the form has been submitted...
@@ -9,9 +10,13 @@ def contact(request):
             print form.cleaned_data["email"]
             # Process the data in form.cleaned_data
             # ...
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
+            return HttpResponseRedirect('/blogfa2wordpress/thanks') # Redirect after POST
     else:
         form = ContactForm() # An unbound form
     return render(request, 'contact.html', {
         'form': form,
     })
+
+
+def thanks(request):
+    return render(request, 'thanks.html')
