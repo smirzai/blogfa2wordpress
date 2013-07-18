cda64750174fb8b8147c43046386db79ba54e555
root <root@server1.saeidmirzaei.com>
2013-07-19 00:16:34 +0400

    update static file directory on server

--- blgf2wrdprsSite/urls.py
+++ blgf2wrdprsSite/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import patterns, include, url
+import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('blgf2wrdprs.urls')),
- 
+(
+        r'^static/(?P<path>.*)$',
+            'django.views.static.serve',
+        {'document_root': settings.STATIC_ROOT}
+        )
 )
